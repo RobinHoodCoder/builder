9094998c7d4f2f09a5fd008d192f2fa86c05c2a2
RobinHoodCoder <robinsteinbach@live.nl>
2021-11-11 11:55:01 +0100

    do more thing

---
+++ src/components/Ingredients/Search.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState, memo } from 'react';
+
+import Card from '../UI/Card';
+import './Search.css';
+
+const url = 'https://react-hooks-update-76090-default-rtdb.europe-west1.firebasedatabase.app/react-hooks-update-76090-default-rtdb.json';
+
+// eslint-disable-next-line react/display-name
+const Search = memo((props) => {
+  const { onLoadIngredients } = props;
+  const [enteredFilter, setEnteredFilter] = useState('');
+
+  useEffect(() => {
+    const query = !!enteredFilter?.length ? `?orderBy="title"&equalTo="${enteredFilter}"` : '';
+    fetch(url + query)
+      .then(response => response.json())
+      .then((data) => {
+        const loadedIngredients = [];
+        Object.entries(data).forEach(([key, value]) => {
+          loadedIngredients.push({
+            id: key,
+            amount: value.amount,
+            title: value.title,
+          });
+        });
+        onLoadIngredients(loadedIngredients);
+      });
+  }, [enteredFilter, onLoadIngredients]);
+  return (
+    <section className="search">
+      <Card>
+        <div className="search-input">
+          <label>Filter by Title</label>
+          <input
+            type="text"
+            value={enteredFilter}
+            onChange={e => setEnteredFilter(e.target.value)}
+          />
+        </div>
+      </Card>
+    </section>
+  );
+});
+
+export default Search;
