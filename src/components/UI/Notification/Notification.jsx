5168a6fbdc5a87f5b4e45e1b49b61607a2641589
RobinHoodCoder <robinsteinbach@live.nl>
2021-11-23 10:59:38 +0100

    notifiy

--- src/components/UI/Notification/Notification.jsx
+++ src/components/UI/Notification/Notification.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import './Notification.module.scss';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-function Notification() {
+import styles from './Notification.module.scss';
+
+function Notification(props) {
+  const { title, message, status } = props;
+  const [key, setKey] = useState(null);
+
+  const classes = [
+    styles.notification,
+    (styles[status] && styles[status]),
+  ].filter(Boolean).join(' ');
+
+  useEffect(() => {
+    setKey(Math.random());
+  }, [status]);
+
   return (
-    <div>
-
+    <div key={key} className={classes}>
+      <p>{title}</p>
+      <p>{message}</p>
     </div>
   );
 }
 
 export default Notification;
+
+Notification.propTypes = {
+  title: PropTypes.string,
+  message: PropTypes.string,
+  status: PropTypes.string,
+};
