c80185e453444644d78f4054aa07a4b0f1d0181c
RobinHoodCoder <robinsteinbach@live.nl>
2021-11-25 11:13:06 +0100

    cart slice test

--- src/redux/cart/cart-slice.test.jsx
+++ src/redux/cart/cart-slice.test.jsx
@@ -1,8 +1,20 @@
+import store, { cartActions } from '../index';
 
-import { useDispatch, useSelector } from 'react-redux';
-import store from '../index';
+describe('Adds a new book', () => {
+  it('', () => {
+    let state = store.getState().cart;
+    const initialProductCount = state.products.length;
 
-test('Adds a new book', () => {
-  const state = store.getState().cart;
-  const initialProductCount = state.products.length;
+    store.dispatch(cartActions.addProduct({ id: '4', title: 'Tester', description: 'Testers manual' }));
+
+    state = store.getState().cart;
+
+    expect(state.products)
+      .toHaveLength(1);
+
+    const addedProduct = state.products.find(prdct => prdct.id === '4');
+
+    expect(addedProduct?.title)
+      .toBe('Tester');
+  });
 });
